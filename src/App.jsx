778605e33794bec17e5b0feb9c9ab6e569c94bbd
Luis Filipe Guimarães Costa <luisfilipegc@hotmail.com>
2024-09-12 16:02:05 -0300

    feat: base CSS structure. feat: base html structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [donateValue, setDonateValue] = useState();
+
+  const handleInput = (e) => {
+    setDonateValue(e.target.value)
+  }
 
   return (
-    <>
+    <section className="container">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+        <div>
+          <button>Donate monthly</button>
+          <button>Donate once</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div>
+        <div>
+          <h3>
+            I would like to make a *monthly" donation of
+          </h3>
+
+          <div>
+            <form onChange={handleInput} >
+
+              <label htmlFor="">
+                2
+                <input type="radio" name="donation" value="2" />
+              </label>
+
+              <label htmlFor="">3
+              <input type="radio" name="donation" value="2" />
+              </label>
+
+              <label htmlFor="">
+                4
+              <input type="radio" name="donation" value="2" />
+              </label>
+            </form>
+          </div>
+
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </section>
   )
 }
 
