fdbe7dad712237c4bf1bd0acf0fb9d6abd479d58
Luis Filipe Guimarães Costa <luisfilipegc@hotmail.com>
2024-09-12 16:22:39 -0300

    feat: base structure ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import { useState } from "react"
+import { useState } from "react";
+import visa from "./assets/visa-logo.png";
+import debit from "./assets/direct-debit.png";
+import fundraising from "./assets/fundraising-logo.png";
+import masterCard from "./assets/mastercard-logo.png";
+import paypal from "./assets/paypal-logo.png";
+import pound from "./assets/pound.svg";
+import secure from "./assets/secure.svg";
+import selected from "./assets/selected.svg";
 
 function App() {
 
-  const [donateValue, setDonateValue] = useState();
+  const [donateValue, setDonateValue] = useState(20);
 
-  const handleInput = (e) => {
-    setDonateValue(e.target.value)
-  }
+  console.log(donateValue)
 
   return (
     <section className="container">
@@ -25,7 +31,7 @@ function App() {
           </h3>
 
           <div>
-            <form onChange={handleInput} >
+            <form onChange={(e) => setDonateValue(e.target.value)} >
 
               <label htmlFor="">
                 2
@@ -33,16 +39,41 @@ function App() {
               </label>
 
               <label htmlFor="">3
-              <input type="radio" name="donation" value="2" />
+                <input type="radio" name="donation" value="3" />
               </label>
 
               <label htmlFor="">
                 4
-              <input type="radio" name="donation" value="2" />
+                <input type="radio" name="donation" value="4" />
               </label>
-            </form>
-          </div>
+              <label htmlFor="">
+                5
+                <input type="radio" name="donation" value="5" />
+              </label>
 
+              <button type="submit">Donate <img src={pound} />{donateValue} montly <img src={secure} alt="" /></button>
+
+            </form>
+
+            <div>
+              <p>
+                <img src={pound} />{donateValue} help an Animal Rescue Team make an urgent animal rescue
+              </p>
+            </div>
+          </div>
+          <div>
+            <div>
+
+              <p>I would like to make a one-off donation</p>
+            </div>
+
+            <div>
+        <p>All Direct Debits are protected by the Direct Debit Guarantee.</p>
+        <img src={debit} alt="" />
+        <img src={fundraising} alt="" />
+            </div>
+
+          </div>
         </div>
       </div>
     </section>
